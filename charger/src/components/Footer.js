1984bb3dd29f16c5c2edd18530979cd8d1c9645a
quyentrankhanhha <t9qutr00@students.oamk.fi>
2020-09-30 05:14:42 +0300

    sep30

--- charger/src/components/Footer.js
+++ charger/src/components/Footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main
+  }
+}));
 
 export default function Footer() {
-    return (
-        <div>
-            <footer>&copy; Copyright 2020 OAMK</footer>
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item md={3}>
+          <Typography variant="h6">Find Out More</Typography>
+        </Grid>
+        <Grid item md={6}>
+          <Typography variant="h5">Be the first to know about Us</Typography>
+          <Typography>
+            Subscribe to get exclusive offers on the world's greatest vacations.
+          </Typography>
+        </Grid>
+        <Grid item md={3}>
+          <Typography variant="h6">Contact Us</Typography>
+          <Typography>&copy; Copyright 2020 OAMK</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
