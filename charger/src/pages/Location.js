4e34589b973ae4dd56aca34e0836237118e7fab5
Bui Quoc Huy <t9buqu00@students.oamk.fi>
2020-10-02 10:49:16 +0300

    d

--- charger/src/pages/Location.js
+++ charger/src/pages/Location.js
@@ -18,7 +18,7 @@ class Location extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key:"AIzaSyDOduUSUYX6lFwhxQmx2b3yHifFBAwiHSw" }}
+          bootstrapURLKeys={{ key:"" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
