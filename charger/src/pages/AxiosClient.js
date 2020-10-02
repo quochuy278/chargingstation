96e3b7a6dee9c4bb2f2895d7bfb65828bf57af97
Quoc Huy <55485803+huy270800@users.noreply.github.com>
2020-10-02 13:39:57 +0300

    Delete AxiosClient.js

--- charger/src/pages/AxiosClient.js
+++
@@ -1,8 +0,0 @@
-function getPlugShare() {
-    axios({
-        method:'get',
-        url: 'https://api.plugshare.com/locations/38606'
-    })
-    .then(res => console.log(res.json()))
-    .catch(err => console.log(err));
-}
