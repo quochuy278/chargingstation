9ffedcc28dab2073da646f50d4a908bd7635f078
Bui Quoc Huy <t9buqu00@students.oamk.fi>
2020-10-05 21:51:09 +0300

    Finish map

--- charger/src/api/AxiosClient.js
+++ charger/src/api/AxiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const getMaps = () => {
-    return axios.get("https://api.openchargemap.io/v3/poi/?output=json&latitude=65.012093&longitude=25.465076&maxresults=25&distanceunit=10miles");
+    return axios.get("https://api.openchargemap.io/v3/poi/?output=json&latitude=65.012093&longitude=25.465076&maxresults=50&distanceunit=10miles");
 }
 
 export  { getMaps };
--- charger/src/components/Map.js
+++ charger/src/components/Map.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
 import { getMaps } from '../api/AxiosClient'
+import MapStyles from '../components/MapStyles'
 const containerStyle = {
   width: '100vw',
   height: '100vh'
@@ -14,35 +15,69 @@ const center = {
 
 
 function Map() {
-  const [data , setData] = useState([])
+  const [location , setLocation] = useState([])
+  const [charger,setSelectedCharger] = useState(null);
+
   const getData = ()=> {
     getMaps()
     .then((res)=>{
         console.log(res.data)
-        setData(res.data)
+        setLocation(res.data)
     })
     .catch((err)=> console.log(err))
 }
+
+
+
 useEffect(() => {
   getData();
 }, []);
-  return (
+
+
+console.log(location)
+ 
+return (
     <LoadScript
       googleMapsApiKey="AIzaSyDOduUSUYX6lFwhxQmx2b3yHifFBAwiHSw"
     >
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={14}
+        options={{styles: MapStyles}}
       >
         {/* <Marker position={{lat:65.012093, lng:25.465076 }} /> */}
-       {data.map((charger) => {
-        return <Marker key={charger.AddressInfo.ID} 
+        
+       {location.map((items) => {
+        return <Marker key={items.AddressInfo.ID} 
                  position={{
-                   lat: charger.AddressInfo.Latitude,
-                   lng: charger.AddressInfo.Longitude,
-        }}/>
+                   lat: items.AddressInfo.Latitude,
+                   lng: items.AddressInfo.Longitude,
+        }}
+              onClick={()=> {
+                setSelectedCharger(items);
+              }}
+              icon={{
+                url: '/charger.png',
+                scaledSize: new window.google.maps.Size(25, 25)
+              }}
+        />
        })}
+       {charger && (<InfoWindow 
+        onCloseClick={()=> {
+          setSelectedCharger(null);
+        }}
+         position={{lat: charger.AddressInfo.Latitude,
+                   lng: charger.AddressInfo.Longitude}}
+        >
+
+         <div>
+         <h1>{charger.AddressInfo.Title}</h1>
+         <p>Address: {charger.AddressInfo.AddressLine1}</p>
+         <p>Charger available: {charger.NumberOfPoints}</p>
+         </div>
+       </InfoWindow>)}
+       
       </GoogleMap>
     </LoadScript>
   )
