28bc547dc26e96641ff3906192f60616319e91a9
Bui Quoc Huy <t9buqu00@students.oamk.fi>
2020-10-05 23:56:39 +0300

    fixed

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,40 +1,21 @@
-<<<<<<< HEAD
+
 import React,{useState,useEffect} from 'react'
 import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
 import { getMaps } from '../api/AxiosClient'
 import MapStyles from '../components/MapStyles'
+
+
+
 const containerStyle = {
   width: '100vw',
   height: '100vh'
 };
- 
-=======
-import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-
-<<<<<<<< HEAD:charger/src/pages/Location.js
-import React from 'react'
-import Map from '../components/Map'
-
-export default function Location() {
-  return (
-    <Map />
-  )
-}
-
-========
-const containerStyle = {
-  width: "70%",
-  height: "600px"
-};
-
->>>>>>> master
 const center = {
   lat: 65.012093,
   lng: 25.465076
 };
 
-<<<<<<< HEAD
+
 
 
 function Map() {
@@ -108,36 +89,5 @@ return (
  
 export default React.memo(Map)
 
-=======
-function Map() {
-  const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
-  return (
-    <LoadScript googleMapsApiKey="" /*API key vao day */>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  );
-}
-
-export default React.memo(Map);
->>>>>>>> master:client/src/components/Map.js
->>>>>>> master
